bc21a5f49741031265db569bc6f4c0af9e42fd19
Sajanauk <43530609+Sajanauk@users.noreply.github.com>
2018-11-24 01:55:03 +0530

    Add files via upload

--- event.sql
+++ event.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2018 at 04:50 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: Nov 23, 2018 at 08:45 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,6 +21,57 @@ SET time_zone = "+00:00";
 --
 -- Database: `event`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addv_image`
+--
+
+CREATE TABLE `addv_image` (
+  `addv_id` int(11) NOT NULL,
+  `image_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_owner`
+--
+
+CREATE TABLE `add_owner` (
+  `addv_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advertisement`
+--
+
+CREATE TABLE `advertisement` (
+  `addv_id` int(11) NOT NULL,
+  `keytag` varchar(255) NOT NULL,
+  `titile` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `advertisement`
+--
+
+INSERT INTO `advertisement` (`addv_id`, `keytag`, `titile`, `area`, `description`, `date`) VALUES
+(5, 'gfdsb', 'dd', 'vcx', 'xx', '0000-00-00'),
+(8, 'aa', 'nbvc', 'nnn', 'tfdx', '0000-00-00'),
+(9, 'aa', 'nbvc', 'nnn', 'tfdx', '0000-00-00'),
+(10, 'aa', 'nbvc', 'nnn', 'tfdx', '0000-00-00'),
+(11, 'aa', 'nbvc', 'nnn', 'tfdx', '0000-00-00'),
+(12, 'aa', 'nbvc', 'nnn', 'tfdx', '0000-00-00'),
+(13, 'aa', 'nbvc', 'nnn', 'tfdx', '2018-11-23'),
+(14, 'aa', 'nbvc', 'nnn', 'tfdx', '2018-11-23');
 
 -- --------------------------------------------------------
 
@@ -270,6 +323,25 @@ INSERT INTO `users` (`Id`, `name`, `email`, `password`, `type`) VALUES
 --
 
 --
+-- Indexes for table `addv_image`
+--
+ALTER TABLE `addv_image`
+  ADD PRIMARY KEY (`addv_id`);
+
+--
+-- Indexes for table `add_owner`
+--
+ALTER TABLE `add_owner`
+  ADD PRIMARY KEY (`user_id`,`addv_id`),
+  ADD KEY `addv_id` (`addv_id`);
+
+--
+-- Indexes for table `advertisement`
+--
+ALTER TABLE `advertisement`
+  ADD PRIMARY KEY (`addv_id`);
+
+--
 -- Indexes for table `answer`
 --
 ALTER TABLE `answer`
@@ -340,20 +412,47 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `advertisement`
+--
+ALTER TABLE `advertisement`
+  MODIFY `addv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT for table `customerorders`
 --
 ALTER TABLE `customerorders`
   MODIFY `OrderNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `QuestionNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `QuestionNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `addv_image`
+--
+ALTER TABLE `addv_image`
+  ADD CONSTRAINT `addv_image_ibfk_1` FOREIGN KEY (`addv_id`) REFERENCES `advertisement` (`addv_id`);
+
+--
+-- Constraints for table `add_owner`
+--
+ALTER TABLE `add_owner`
+  ADD CONSTRAINT `add_owner_ibfk_1` FOREIGN KEY (`addv_id`) REFERENCES `advertisement` (`addv_id`),
+  ADD CONSTRAINT `add_owner_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
